f1e41059284420728429d37adcde433b4f145a7f
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-04 01:19:00 +0200

    Add server code

---
+++ server.mjs
@@ -0,0 +1,24 @@
+import { readFileSync } from 'node:fs'
+import { createServer } from 'node:http'
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = createServer((req, res) => {
+	if (req.url === '/') {
+		res.statusCode = 200
+		res.setHeader('Content-Type', 'text/html')
+		res.end(readFileSync('./test/index.html', 'utf-8'))
+	} else if (req.url?.endsWith('.mjs')) {
+		res.statusCode = 200
+		res.setHeader('Content-Type', 'application/javascript')
+		res.end(readFileSync('./dist/index.mjs', 'utf-8'))
+	} else {
+		res.statusCode = 404
+		res.end()
+	}
+})
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`)
+})
