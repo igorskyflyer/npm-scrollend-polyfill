2a208bdc77617812c6db337ce7fb7c01788c45c4
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-09 01:52:32 +0200

    Move to test

---
+++ test/server.mjs
@@ -0,0 +1,24 @@
+import { readFileSync } from 'node:fs'
+import { createServer } from 'node:http'
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = createServer((req, res) => {
+	if (req.url === '/') {
+		res.statusCode = 200
+		res.setHeader('Content-Type', 'text/html')
+		res.end(readFileSync('./test/index.html', 'utf-8'))
+	} else if (req.url?.endsWith('.js')) {
+		res.statusCode = 200
+		res.setHeader('Content-Type', 'application/javascript')
+		res.end(readFileSync(`.${req.url}`, 'utf-8'))
+	} else {
+		res.statusCode = 404
+		res.end()
+	}
+})
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`)
+})
