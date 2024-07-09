bd2f60a833f8fdbb4479f9504f5a93df79c50804
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-09 02:31:40 +0200

    Handle file serving

--- test/server.mjs
+++ test/server.mjs
@@ -4,21 +4,36 @@ import { createServer } from 'node:http'
 const hostname = '127.0.0.1'
 const port = 3000
 
+const routes = {
+  js: { contentType: 'application/javascript', path: '.' },
+  css: { contentType: 'text/css', path: './test' }
+}
+
 const server = createServer((req, res) => {
-	if (req.url === '/') {
-		res.statusCode = 200
-		res.setHeader('Content-Type', 'text/html')
-		res.end(readFileSync('./test/index.html', 'utf-8'))
-	} else if (req.url?.endsWith('.js')) {
-		res.statusCode = 200
-		res.setHeader('Content-Type', 'application/javascript')
-		res.end(readFileSync(`.${req.url}`, 'utf-8'))
-	} else {
-		res.statusCode = 404
-		res.end()
-	}
+  let root = './test'
+
+  if (!req || !req.url) {
+    res.statusCode = 404
+    res.end()
+  } else if (req.url === '/') {
+    root = './test'
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(readFileSync(`${root}/index.html`, 'utf-8'))
+  } else {
+    const extension = req.url.substring(req.url.lastIndexOf('.') + 1)
+    const route = routes[extension]
+    const filename = req.url.substring(1)
+
+    root = route.path
+
+    res.setHeader('Content-Type', route.contentType)
+    res.statusCode = 200
+    res.end(readFileSync(`${root}/${filename}`, 'utf-8'))
+  }
 })
 
 server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
